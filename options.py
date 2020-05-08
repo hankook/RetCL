5693f3f3b74fee091be8e0f73c21968c2b959100
Hankook Lee <gksrnr0202@gmail.com>
2020-05-08 10:16:26 +0900

    update

---
+++ options.py
@@ -0,0 +1,8 @@
+
+def add_model_arguments(parser):
+    parser.add_argument('--module', type=str, default='v1')
+    parser.add_argument('--encoder', type=str, default='s2v')
+    parser.add_argument('--num-layers', type=int, default=5)
+    parser.add_argument('--num-branches', type=int, default=2)
+    parser.add_argument('--K', type=int, default=2)
+    parser.add_argument('--bn-first', action='store_true')
